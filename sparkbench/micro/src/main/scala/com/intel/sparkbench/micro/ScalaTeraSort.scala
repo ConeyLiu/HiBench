f524446c0b92d147838b315c46b1f323fa17df1c
Xianyang Liu <xyliu0530@icloud.com>
2016-12-06 11:44:26 +0800

    Modify the code of 'TeraSort' & add new test case 'Repartion'

--- sparkbench/micro/src/main/scala/com/intel/sparkbench/micro/ScalaTeraSort.scala
+++ sparkbench/micro/src/main/scala/com/intel/sparkbench/micro/ScalaTeraSort.scala
@@ -17,13 +17,18 @@
 
 package com.intel.hibench.sparkbench.micro
 
+import java.util.Comparator
+
 import com.intel.hibench.sparkbench.common.IOCommon
+import com.intel.sparkbench.micro.TimSort
 import org.apache.hadoop.examples.terasort.{TeraInputFormat, TeraOutputFormat}
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.io.BytesWritable
 import org.apache.spark._
 import org.apache.spark.rdd._
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 
 object ScalaTeraSort {
@@ -53,29 +58,74 @@ object ScalaTeraSort {
     val reducer  = IOCommon.getProperty("hibench.default.shuffle.parallelism")
       .getOrElse((parallel / 2).toString).toInt
 
+    val nvmfEnabled = sc.getConf.getBoolean("spark.shuffle.nvme.enabled", false)
+
     val partitioner = new BaseRangePartitioner(partitions = reducer, rdd = data)
 
+//    val partitioned_sorted_rdd = data.map(kv => (partitioner.getPartition(kv._1), (kv._1, kv._2)))
+//      .mapPartitions{ iterator =>
+//        iterator.toSeq
+//                .sortWith((r1, r2) => partitionKeyCompare((r1._1, r1._2._1), (r2._1, r2._2._1)) < 0)
+//                .toIterator
+//      }
+
     val partitioned_sorted_rdd = data.map(kv => (partitioner.getPartition(kv._1), (kv._1, kv._2)))
-      .mapPartitions{ iterator =>
-        iterator.toSeq
-                .sortWith((r1, r2) => partitionKeyCompare((r1._1, r1._2._1), (r2._1, r2._2._1)) < 0)
-                .toIterator
+      .mapPartitions { iterator =>
+        val map = new mutable.HashMap[Int, ArrayBuffer[(Array[Byte], Array[Byte])]]()
+        iterator.foreach { case (pid, kv) =>
+            val arr = map.getOrElseUpdate(pid, new ArrayBuffer[(Array[Byte], Array[Byte])]())
+            arr += kv
+        }
+
+        map.foreach{ case (pid, kv) =>
+            kv.sortWith((r1, r2) => keyCompare(r1._1, r2._1) < 0)
+        }
+
+        val arrBuffer = new ArrayBuffer[(Int, (Array[Byte], Array[Byte]))]()
+        map.foreach { case (pid, kvArr) =>
+            kvArr.foreach { case (k, v) =>
+                arrBuffer += ((pid, (k ,v)))
+            }
+        }
+        arrBuffer.toIterator
       }
 
     val ordered_rdd = new TeraSortPairRDDFunctions(partitioned_sorted_rdd)
 
-    val sorted_rdd = ordered_rdd.groupByKey(new HashPartitioner(partitioner.numPartitions)).flatMap{ tuple =>
-        val value = tuple._2
-        val sortedIterator = value.toSeq.sortWith { (v1, v2) =>
-          keyCompare(v1._1, v2._2) < 0
-        }
-        sortedIterator
-    }.map{case (k, v) => (new Text(k), new Text(v))}
+    val hashPartitioner = new HashPartitioner(partitioner.numPartitions)
 
-    sorted_rdd.saveAsNewAPIHadoopFile[TeraOutputFormat](args(1))
+    val grouped_rdd = ordered_rdd.groupByKey(hashPartitioner)
 
+    if (nvmfEnabled) {
+      grouped_rdd.flatMap(_._2.toIterator)
+        .map { case (k, v) => (new Text(k), new Text(v))}
+        .saveAsNewAPIHadoopFile[TeraOutputFormat](args(1))
+    } else {
+      grouped_rdd.flatMap{ tuple =>
+        val value = tuple._2.toArray
+
+        TimSort.sort(value,
+                     0,
+                     value.length,
+                     keyComparator(),
+                     new Array[(Array[Byte], Array[Byte])](value.length),
+                     value.length)
+
+        value
+      }.map{case (k, v) => (new Text(k), new Text(v))}
+        .saveAsNewAPIHadoopFile[TeraOutputFormat](args(1))
+
+    }
 
     sc.stop()
+  }
+
+  def keyComparator(): Comparator[(Array[Byte], Array[Byte])] = {
+    new Comparator[(Array[Byte], Array[Byte])] {
+      override def compare(a: (Array[Byte], Array[Byte]), b: (Array[Byte], Array[Byte])): Int = {
+        keyCompare(a._1, b._1)
+      }
+    }
   }
 
 
